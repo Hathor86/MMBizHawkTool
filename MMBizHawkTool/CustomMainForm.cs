d3cceeeb55e266c61535dc7bb9a3d19a5628bcdb
Hathor86 <francois.guiot@gmail.com>
2015-10-02 21:07:32 +0200

    Create initial empty project

---
+++ MMBizHawkTool/CustomMainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BizHawk.Client.EmuHawk
+{
+    public partial class CustomMainForm : Form, IToolForm, ICustomGameTool
+    {
+        public CustomMainForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool UpdateBefore
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool AskSaveChanges()
+        {
+            return true;
+        }
+
+        public void FastUpdate()
+        {
+           
+        }
+
+        public void Restart()
+        {
+            
+        }
+
+        public void UpdateValues()
+        {
+           
+        }
+    }
+}
